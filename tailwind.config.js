ebcddd968ba7d340e0b52d55503f1169162106dd
nurinno <uenal@innoscripta.com>
2025-01-26 12:33:12 +0100

    tailwind config fix and deleting the unnecesary lesson editor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ['./src/**/*.{html,js}'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
             borderRadius: {
@@ -93,5 +93,8 @@ module.exports = {
             }
         }
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('tailwindcss-animate')
+    ],
 };
